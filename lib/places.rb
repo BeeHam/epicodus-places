6a38dee351db95606591fc645f1ea793c06d4c7a
Brigham Thompson & Clinton Herrick <student@epicodus.com>
2015-08-11 12:10:42 -0700

    adds initialize & destination method

--- lib/places.rb
+++ lib/places.rb
@@ -1,3 +1,11 @@
 class Places
-  # code
+  @@all_places = []
+
+  define_method(:initialize) do |destination|
+    @destination = destination
+  end
+
+  define_method(:destination) do
+    @destination
+  end
 end
